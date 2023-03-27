60e051e8669ff48f4019e02e559d0cdc5054dc79
codeTeu <teu@sheridancollege.ca>
2023-03-27 00:30:27 -0400

    Initial Commit

---
+++ src/main/java/ca/marjorieteu/BarkpointmentApplication.java
@@ -0,0 +1,13 @@
+package ca.marjorieteu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarkpointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarkpointmentApplication.class, args);
+	}
+
+}
