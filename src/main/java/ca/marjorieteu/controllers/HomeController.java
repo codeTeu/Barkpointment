eed2853415d910c765d69f831b58249e9d081c76
codeTeu <teu@sheridancollege.ca>
2023-03-27 01:50:58 -0400

    added thymeleaf
    connected scripts, css, bootstrap, jquery

--- src/main/java/ca/marjorieteu/controllers/HomeController.java
+++ src/main/java/ca/marjorieteu/controllers/HomeController.java
@@ -1,8 +1,14 @@
 package ca.marjorieteu.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+	@GetMapping("/")
+	public String goHome() {
+		return "index";
+	}
 
 }
