d15f92eaf7f01c53750837c163260feabf42ce57
codeTeu <teu@sheridancollege.ca>
2023-03-27 22:24:27 -0400

    created base html on all pages added mapping for all pages

--- src/main/java/ca/marjorieteu/controllers/HomeController.java
+++ src/main/java/ca/marjorieteu/controllers/HomeController.java
@@ -11,4 +11,43 @@ public class HomeController {
 		return "index";
 	}
 
+	@GetMapping("/login")
+	public String goLogin() {
+		return "login";
+	}
+
+	@GetMapping("/register")
+	public String goRegister() {
+		return "register";
+	}
+
+	@GetMapping("/contact")
+	public String goContact() {
+		return "contact";
+	}
+
+	@GetMapping("/bookAppt")
+	public String goBookAppt() {
+		return "secured/index";
+	}
+
+	@GetMapping("/profile")
+	public String goProfile() {
+		return "secured/profile";
+	}
+
+	@GetMapping("/pets")
+	public String goPets() {
+		return "secured/pets";
+	}
+
+	@GetMapping("/addPet")
+	public String goAddPet() {
+		return "secured/addPet";
+	}
+
+	@GetMapping("/appointments")
+	public String goAppts() {
+		return "secured/appointments";
+	}
 }
